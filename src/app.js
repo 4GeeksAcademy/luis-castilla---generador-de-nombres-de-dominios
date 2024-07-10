b1e260c31496e3a6c00cbc4bb1de54bf8ae2e731
luiscastillaPro <159273408+luiscastillaPro@users.noreply.github.com>
2024-07-10 09:33:19 +0000

    terminadoo

--- src/app.js
+++ src/app.js
@@ -8,8 +8,9 @@ for (let i = 0; i < pronoun.length; i++) {
   for (let x = 0; x < adj.length; x++) {
     for (let z = 0; z < noun.length; z++) {
       for (let h = 0; h < extenciones.length; h++) {
-        console.log(nuevo.push(pronoun[i] + adj[x] + noun[z] + extenciones[h]));
+        nuevo.push(pronoun[i] + adj[x] + noun[z] + extenciones[h]);
       }
     }
   }
 }
+console.log(nuevo);
