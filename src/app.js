676a31cdbddf327807defb2221866b416973de09
luiscastillaPro <159273408+luiscastillaPro@users.noreply.github.com>
2024-07-10 09:32:11 +0000

    terminado

--- src/app.js
+++ src/app.js
@@ -1,11 +1,15 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+let pronoun = ["the", "our"];
+let adj = ["great", "big"];
+let noun = ["jogger", "racoon"];
+let extenciones = [".com", ".net"];
+let nuevo = [];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+for (let i = 0; i < pronoun.length; i++) {
+  for (let x = 0; x < adj.length; x++) {
+    for (let z = 0; z < noun.length; z++) {
+      for (let h = 0; h < extenciones.length; h++) {
+        console.log(nuevo.push(pronoun[i] + adj[x] + noun[z] + extenciones[h]));
+      }
+    }
+  }
+}
